170b3f8bd12d4bc38df6534618c7cf1b651f2dbc
carlo@piana.eu <carlo@piana.eu>
2018-01-19 09:53:45 +0100

    clarification

--- hashline.sh
+++ hashline.sh
@@ -3,10 +3,11 @@
 # Use case: A wants to claim they can trade with their clients but they don't want
 # to release information. Client gives the hashed list. If there is any challenge
 # to that, the non-revealing party just gives the name and the other party can
-# verify that the clientalready is listed. That does not copromise the entire
+# verify that the clientalready is listed. That does not compromise the entire
 # list.
 
 while read line; do echo -n $line|sha256sum; done < $1 |awk '{print $1}'
 
 # to check a single line:
-# echo -n "[hashed string]" | sha256 sum | awk '{print $1}' 
+# echo -n "[hashed string]" | sha256 sum | awk '{print $1}'
+# "echo -n" avoids reading the newline at the end of each line
